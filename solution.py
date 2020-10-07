bcd9874dc3d627a46aeeb9d9ae18c67d5ae18c46
xw2535 <email@example.com>
2020-10-07 12:20:08 -0400

    6

--- solution.py
+++ solution.py
@@ -6,13 +6,13 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mailServer ='127.0.0.1'
-    mailPort=1025
+    #mailServer ='127.0.0.1'
+    #mailPort=1025
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     
     # Fill in start
     clientSocket = socket(AF_INET,SOCK_STREAM)
-    clientSocket.connect(mailServer, mailPort)
+    clientSocket.connect(mailserver, port)
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
