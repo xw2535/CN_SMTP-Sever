c0f1f3cc581ed3a818da903330b59615bd704b83
xw2535 <email@example.com>
2020-10-07 12:13:59 -0400

    mailv4

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     
     # Fill in start
     clientSocket = socket(AF_INET,SOCK_STREAM)
-    clientSocket.connect((mailServer, mailPort))
+    clientSocket.connect(mailServer, mailPort)
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
